4fc52b3aa1e4ff25c62016661ed5a1af71b31ab6
Anthony.M <marcellin.anthony77@gmail.com>
2023-03-27 02:44:08 +0200

    Implémentation des tests unitaires et d'intégrations Bills.js

--- client/src/__tests__/Bills.js
+++ client/src/__tests__/Bills.js
@@ -3,17 +3,20 @@
  */
 
 import { screen, waitFor } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect.js';
 
+// Mocks
+import { localStorageMock } from '../__mocks__/localStorage.js';
+import mockStore from '../__mocks__/store.js';
+jest.mock('../app/store', () => mockStore);
+
+// Data & Interfaces requirements
 import BillsUI from '../views/BillsUI.js';
 import { bills } from '../fixtures/bills.js';
-import { ROUTES_PATH } from '../constants/routes.js';
-import { localStorageMock } from '../__mocks__/localStorage.js';
-
-import router from '../app/Router.js';
 import Bills from '../containers/Bills.js';
-import { ROUTES } from '../constants/routes.js';
-import userEvent from '@testing-library/user-event';
+import { ROUTES_PATH, ROUTES } from '../constants/routes.js';
+import router from '../app/Router.js';
 
 describe('Given I am connected as an employee', () => {
     describe('When I am on Bills Page', () => {
@@ -116,4 +119,76 @@ describe('Given I am connected as an employee', () => {
             expect(modale).toBeTruthy();
         })
     })
+});
+
+// test d'intégration GET
+describe('Given I am a user connected as Employee', () => {
+    describe('When I navigate to Bills', () => {
+        test('fetches bills from mock API GET', async () => {
+            localStorage.setItem('user', JSON.stringify({
+                type: 'Employee',
+                email: 'e@e'
+            }));
+
+            const root = document.createElement('div');
+            root.setAttribute('id', 'root');
+            document.body.append(root);
+            router();
+            window.onNavigate(ROUTES_PATH.Bills);
+
+            await waitFor(() => screen.getByText('Mes notes de frais'));
+            const asBeenBillsPage = await screen.getByText('Mes notes de frais');
+            expect(asBeenBillsPage).toBeTruthy();
+
+        });
+        describe('When an error occurs on API', () => {
+            beforeEach(() => {
+                jest.spyOn(mockStore, 'bills');
+                Object.defineProperty(
+                    window,
+                    'localStorage',
+                    {value: localStorageMock},
+                );
+                window.localStorage.setItem('user', JSON.stringify({
+                    type: 'Employee',
+                    email: 'e@e',
+                }));
+                const root = document.createElement('div');
+                root.setAttribute('id', 'root');
+                document.body.appendChild(root);
+                router();
+            });
+            test('fetches bills from an API and fails with 404 message error', async () => {
+
+                mockStore.bills.mockImplementationOnce(() => {
+                    return {
+                        list: () => {
+                            return Promise.reject(new Error('Erreur 404'));
+                        },
+                    };
+                });
+                window.onNavigate(ROUTES_PATH.Bills);
+                await new Promise(process.nextTick);
+                const message = await screen.getByText(/Erreur 404/);
+                expect(message).toBeTruthy();
+            });
+
+            test('fetches messages from an API and fails with 500 message error', async () => {
+
+                mockStore.bills.mockImplementationOnce(() => {
+                    return {
+                        list: () => {
+                            return Promise.reject(new Error('Erreur 500'));
+                        },
+                    };
+                });
+
+                window.onNavigate(ROUTES_PATH.Bills);
+                await new Promise(process.nextTick);
+                const message = await screen.getByText(/Erreur 500/);
+                expect(message).toBeTruthy();
+            });
+        });
+
+    });
 });
